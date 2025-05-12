85626f2ca7ec1f421a729bd09c126dc6264d1116
davo9 <davidmejia0951@gmail.com>
2025-05-13 01:53:28 +0200

    creacion de la clase usuario

---
+++ src/model/usuario.py
@@ -0,0 +1,7 @@
+class Usuario:
+    def __init__(self, cedula: int, nombre: str, apellido: str, edad: int, genero: str):
+        self.cedula = cedula
+        self.nombre = nombre
+        self.apellido = apellido
+        self.edad = edad
+        self.genero = genero
