bfba6d41897e2a96f8219fe0f62a9f969c232ba1
David Hernandez Mejia <davidmejia0951@gmail.com>
2025-05-12 23:17:24 -0500

    creacion de archivo salarios

--- src/controller/usuarios_controller.py
+++ src/controller/usuarios_controller.py
@@ -23,7 +23,7 @@ class UsuariosController:
             consulta = archivo.read()
 
         cursor.execute(consulta)
-        cursor.connection.commit()        
+        cursor.connection.commit()
 
     def obtener_cursos():
         conection = psycopg2.connect(database = SecretConfig.PGDATABASE, user = SecretConfig.PGUSER, password = SecretConfig.PGPASSWORD, host = SecretConfig.PGHOST, port = SecretConfig.PGPORT)
