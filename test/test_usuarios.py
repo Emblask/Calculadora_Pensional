c2b19fdd6ed865e2cc6fc1b21c4faa9eb32d7305
David Hernandez Mejia <davidmejia0951@gmail.com>
2025-05-13 02:05:56 -0500

    test_insert_error creado

--- test/test_usuarios.py
+++ test/test_usuarios.py
@@ -13,7 +13,7 @@ class TestTarjeta(unittest.TestCase):
         UsuariosController.borrar_tabla()
         UsuariosController.crear_tabla()
 
-    def test_insert_1(self):
+    def test_insert_normal_1(self):
         usuario = Usuario(  cedula = 1022145818,
                             nombre = "David",
                             apellido = "Hernandez",
@@ -23,6 +23,24 @@ class TestTarjeta(unittest.TestCase):
         UsuariosController.insertar(usuario)
         usuario_buscado = UsuariosController.buscar_usuario(usuario.cedula)
         self.assertTrue(usuario_buscado.EsIgual(usuario))
+    
+    def test_insert_error_2(self):
+        usuario =  Usuario( cedula = 43669647,
+                            nombre = "Isabel",
+                            apellido = "Trespalacios",
+                            edad = 68,
+                            genero = "Femenino")
+        
+        usuario_2 =  Usuario(  cedula = 8401133,
+                            nombre = "Rafael",
+                            apellido = "Hernandez",
+                            edad = 59,
+                            genero = "Masculino")
+        
+        UsuariosController.insertar(usuario)
+        UsuariosController.insertar(usuario_2)
+        Usuario_buscado = UsuariosController.buscar_usuario(usuario_2.cedula)
+        self.assertFalse(usuario.EsIgual(Usuario_buscado))
 
 
 if __name__ == "__main__":
