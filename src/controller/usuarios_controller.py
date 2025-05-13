45d6b0d72b3dd505aa73e65b7774bd4999a9957b
David Hernandez Mejia <davidmejia0951@gmail.com>
2025-05-12 23:45:36 -0500

    creacion del metodo insertar usuario

--- src/controller/usuarios_controller.py
+++ src/controller/usuarios_controller.py
@@ -5,25 +5,29 @@ sys.path.append("src")
 sys.path.append(".")
 
 import SecretConfig
+from model.usuario import Usuario
 
 class UsuariosController:
     def crear_tabla():
         cursor = UsuariosController.obtener_cursos()
-
         with open("sql/crear_usuarios.sql", "r") as archivo:
             consulta = archivo.read()
-        
         cursor.execute(consulta)
         cursor.connection.commit()
 
     def borrar_tabla():
         cursor = UsuariosController.obtener_cursos()
-
         with open("sql/borrar_usuarios.sql", "r") as archivo:
             consulta = archivo.read()
-
         cursor.execute(consulta)
         cursor.connection.commit()
+    
+    def insertar(usuario: Usuario):
+        cursor = UsuariosController.obtener_cursos()
+        consulta = f"insert into usuarios values ('{usuario.cedula}', '{usuario.nombre}', '{usuario.apellido}', '{usuario.edad}', '{usuario.edad}')"
+        cursor.execute(consulta)
+        cursor.connection.commit()
+    
 
     def obtener_cursos():
         conection = psycopg2.connect(database = SecretConfig.PGDATABASE, user = SecretConfig.PGUSER, password = SecretConfig.PGPASSWORD, host = SecretConfig.PGHOST, port = SecretConfig.PGPORT)
--- test/test_usuarios.py
+++ test/test_usuarios.py
@@ -8,8 +8,14 @@ from model.usuario import Usuario
 from controller.usuarios_controller import UsuariosController
 
 class TestTarjeta(unittest.TestCase):
+
     def setUpClass():
         UsuariosController.borrar_tabla()
         UsuariosController.crear_tabla()
 
-#Continuar con las pruebas
+    def test_insert_1(self):
+        usuario = Usuario(  cedula = 1022145818,
+                            nombre = "David",
+                            apellido = "Hernandez",
+                            edad = 71,
+                            genero = "Masculino")
