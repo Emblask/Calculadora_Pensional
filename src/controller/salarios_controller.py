dfdf08ac19acaed18ae07a94e38eb44cbd88e848
simon12090234 <corrasimon82@gmail.com>
2025-05-13 22:58:27 -0500

    creacion del metodo insertar salarios

--- src/controller/salarios_controller.py
+++ src/controller/salarios_controller.py
@@ -5,30 +5,41 @@ sys.path.append("src")
 sys.path.append(".")
 
 import SecretConfig
+from model.salarios import Salarios
 
-class Controlar_Salarios:
+class SalariosController:
     
     def crear_tabla_salarios():
-        cursor = Controlar_Salarios.obtener_cursor()
+        cursor = SalariosController.obtener_cursor()
+
         with open("sql/crear_salarios.sql", "r") as archivo:
             consulta = archivo.read()
         cursor.execute(consulta)
         cursor.connection.commit()
     
-    def obtener_cursor():
-        conection = psycopg2.connect(database = SecretConfig.PGDATABASE, user = SecretConfig.PGUSER, password = SecretConfig.PGPASSWORD, host = SecretConfig.PGHOST, port = SecretConfig.PGPORT)
-        cursor = conection.cursor()
-        return cursor
-    
     def borrar_tabla_salarios():
-        cursor = Controlar_Salarios.obtener_cursor()
+        cursor = SalariosController.obtener_cursor()
+
         with open("sql/borrar_salarios.sql", "r") as archivo:
             consulta = archivo.read()
         cursor.execute(consulta)
         cursor.connection.commit()
 
-    def insertar_datos_salarios():
-        ...
+    def insertar_datos_salarios(salarios: Salarios):
+        cursor = SalariosController.obtener_cursor()
 
+        consulta = f"insert into salarios values ('{salarios.lista_salarios[0]}', '{salarios.lista_salarios[1]}', '{salarios.lista_salarios[2]}', '{salarios.lista_salarios[3]}', '{salarios.lista_salarios[4]}', '{salarios.lista_salarios[5]}', '{salarios.lista_salarios[6]}', '{salarios.lista_salarios[7]}', '{salarios.lista_salarios[8]}', '{salarios.lista_salarios[9]}')"
+        cursor.execute(consulta)
+        cursor.connection.commit()
+
+    def obtener_cursor():
+        conection = psycopg2.connect(database = SecretConfig.PGDATABASE, user = SecretConfig.PGUSER, password = SecretConfig.PGPASSWORD, host = SecretConfig.PGHOST, port = SecretConfig.PGPORT)
+        cursor = conection.cursor()
+        return cursor
+    
+    
 if __name__ == "__main__":
-    Controlar_Salarios.crear_tabla_salarios()
+    SalariosController.crear_tabla_salarios()
+    
+
+    
