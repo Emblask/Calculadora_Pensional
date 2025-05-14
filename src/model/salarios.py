ca03ae70a376265bd36a519feffacc0ee94f50b4
simon12090234 <corrasimon82@gmail.com>
2025-05-13 23:17:23 -0500

    se cambia la logica de lista de salarios a salarios individuales

--- src/model/salarios.py
+++ src/model/salarios.py
@@ -1,29 +1,39 @@
 class Salarios:
-    def __init__(self,cedula: int,  lista_salarios: list[int]):
+    def __init__(self,cedula: int,  salario_1 : int, salario_2 : int, salario_3 : int, salario_4 : int, salario_5 : int, salario_6 : int, salario_7 : int, salario_8 : int, salario_9 : int, salario_10 : int):
         self.cedula = cedula
-        self.lista_salarios = lista_salarios
+        self.salario_1 = salario_1
+        self.salario_2 = salario_2
+        self.salario_3 = salario_3
+        self.salario_4 = salario_4
+        self.salario_5 = salario_5
+        self.salario_6 = salario_6
+        self.salario_7 = salario_7
+        self.salario_8 = salario_8
+        self.salario_9 = salario_9
+        self.salario_10 = salario_10
     
     def EsIgual(self, otro):
         if self.cedula != otro.cedula:
             return False
-        if self.lista_salarios[0] != otro.lista_salarios[0]:
+        if self.salario_1 != otro.salario_1:
             return False
-        if self.lista_salarios[1] != otro.lista_salarios[1]:
+        if self.salario_2 != otro.salario_2:
             return False
-        if self.lista_salarios[2] != otro.lista_salarios[2]:
+        if self.salario_3 != otro.salario_3:
             return False
-        if self.lista_salarios[3] != otro.lista_salarios[3]:
+        if self.salario_4 != otro.salario_4:
             return False
-        if self.lista_salarios[4] != otro.lista_salarios[4]:
+        if self.salario_5 != otro.salario_5:
             return False
-        if self.lista_salarios[5] != otro.lista_salarios[5]:
+        if self.salario_6 != otro.salario_6:
             return False
-        if self.lista_salarios[6] != otro.lista_salarios[6]:
+        if self.salario_7 != otro.salario_7:
             return False
-        if self.lista_salarios[7] != otro.lista_salarios[7]:
+        if self.salario_8 != otro.salario_8:
             return False
-        if self.lista_salarios[8] != otro.lista_salarios[8]:
+        if self.salario_9 != otro.salario_9:
             return False
-        if self.lista_salarios[9] != otro.lista_salarios[9]:
+        if self.salario_10 != otro.salario_10:
             return False
+
         return True
