ffa72961e53ca3a3ef882070f5ec35a133bf1a6d
simon12090234 <corrasimon82@gmail.com>
2025-05-13 23:01:19 -0500

    correccion del uso de la cedula y el metodo comparar

--- src/model/salarios.py
+++ src/model/salarios.py
@@ -1,26 +1,29 @@
 class Salarios:
-    def __init__(self, lista_salarios: list[int]):
+    def __init__(self,cedula: int,  lista_salarios: list[int]):
+        self.cedula = cedula
         self.lista_salarios = lista_salarios
     
     def EsIgual(self, otro):
-        if self.lista_salarios[0] == otro.lista_salarios[0]:
+        if self.cedula != otro.cedula:
             return False
-        if self.lista_salarios[1] == otro.lista_salarios[1]:
+        if self.lista_salarios[0] != otro.lista_salarios[0]:
             return False
-        if self.lista_salarios[2] == otro.lista_salarios[2]:
+        if self.lista_salarios[1] != otro.lista_salarios[1]:
             return False
-        if self.lista_salarios[3] == otro.lista_salarios[3]:
+        if self.lista_salarios[2] != otro.lista_salarios[2]:
             return False
-        if self.lista_salarios[4] == otro.lista_salarios[4]:
+        if self.lista_salarios[3] != otro.lista_salarios[3]:
             return False
-        if self.lista_salarios[5] == otro.lista_salarios[5]:
+        if self.lista_salarios[4] != otro.lista_salarios[4]:
             return False
-        if self.lista_salarios[6] == otro.lista_salarios[6]:
+        if self.lista_salarios[5] != otro.lista_salarios[5]:
             return False
-        if self.lista_salarios[7] == otro.lista_salarios[7]:
+        if self.lista_salarios[6] != otro.lista_salarios[6]:
             return False
-        if self.lista_salarios[8] == otro.lista_salarios[8]:
+        if self.lista_salarios[7] != otro.lista_salarios[7]:
             return False
-        if self.lista_salarios[9] == otro.lista_salarios[9]:
+        if self.lista_salarios[8] != otro.lista_salarios[8]:
+            return False
+        if self.lista_salarios[9] != otro.lista_salarios[9]:
             return False
         return True
