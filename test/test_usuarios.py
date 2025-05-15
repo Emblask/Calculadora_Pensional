01f08081a4ef357ed3d496f2ac440e0f3d94b27f
David Hernandez Mejia <davidmejia0951@gmail.com>
2025-05-14 23:55:54 -0500

    se implementas los 10 salarios

--- test/test_usuarios.py
+++ test/test_usuarios.py
@@ -20,7 +20,17 @@ class TestTarjeta(unittest.TestCase):
                             apellido = "Hernandez",
                             edad = 71,
                             genero = "Masculino",
-                            numero_hijos = 1)
+                            numero_hijos = 1,
+                            salario_1 = 4000000,
+                            salario_2 = 3100000,
+                            salario_3 = 3200000,
+                            salario_4 = 3300000,
+                            salario_5 = 3400000,
+                            salario_6 = 3500000,
+                            salario_7 = 3600000,
+                            salario_8 = 3700000,
+                            salario_9 = 3800000,
+                            salario_10 = 3900000)
         
         UsuariosController.insertar(usuario)
         usuario_buscado = UsuariosController.buscar_usuario(usuario.cedula)
@@ -32,14 +42,34 @@ class TestTarjeta(unittest.TestCase):
                             apellido = "Trespalacios",
                             edad = 68,
                             genero = "Femenino",
-                            numero_hijos = 2)
+                            numero_hijos = 2,
+                            salario_1 = 4500000,
+                            salario_2 = 8100000,
+                            salario_3 = 1200000,
+                            salario_4 = 3000000,
+                            salario_5 = 3000000,
+                            salario_6 = 3000000,
+                            salario_7 = 5000000,
+                            salario_8 = 2700000,
+                            salario_9 = 2000000,
+                            salario_10 = 3900000)
         
         usuario_2 =  Usuario(  cedula = 8401133,
                             nombre = "Rafael",
                             apellido = "Hernandez",
                             edad = 59,
                             genero = "Masculino",
-                            numero_hijos = 0)
+                            numero_hijos = 0,
+                            salario_1 = 4000000,
+                            salario_2 = 7100000,
+                            salario_3 = 7200000,
+                            salario_4 = 1300000,
+                            salario_5 = 8400000,
+                            salario_6 = 5500000,
+                            salario_7 = 5600000,
+                            salario_8 = 5600000,
+                            salario_9 = 1800000,
+                            salario_10 = 5000000)
         
         UsuariosController.insertar(usuario)
         UsuariosController.insertar(usuario_2)
@@ -52,7 +82,17 @@ class TestTarjeta(unittest.TestCase):
                             apellido = "Pote",
                             edad = 71,
                             genero = "Femenino",
-                            numero_hijos = 2)
+                            numero_hijos = 2,
+                            salario_1 = 4000000,
+                            salario_2 = 3100000,
+                            salario_3 = 3200000,
+                            salario_4 = 3300000,
+                            salario_5 = 3400000,
+                            salario_6 = 3500000,
+                            salario_7 = 3600000,
+                            salario_8 = 3700000,
+                            salario_9 = 3800000,
+                            salario_10 = 3900000)
         
         UsuariosController.insertar(usuario)
         UsuariosController.eliminar(usuario.cedula)
