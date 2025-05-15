c1bac04f5c2fff19e7da1c5ff12f23bea00595ab
David Hernandez Mejia <davidmejia0951@gmail.com>
2025-05-14 23:31:15 -0500

    actualizacion de la tabla usuarios para ingresar los 10 salarios

--- sql/crear_usuarios.sql
+++ sql/crear_usuarios.sql
@@ -4,5 +4,16 @@ create table if not exists usuarios (
     apellido varchar(15) not null,
     edad varchar(15) not null,
     genero varchar(10) not null,
-    numero_hijos Integer not null
+    numero_hijos Integer not null,
+    salario_ano1 NUMERIC(12,2) not null,
+    salario_ano2 NUMERIC(12,2) not null,
+    salario_ano3 NUMERIC(12,2) not null,
+    salario_ano4 NUMERIC(12,2) not null,
+    salario_ano5 NUMERIC(12,2) not null,
+    salario_ano6 NUMERIC(12,2) not null,
+    salario_ano7 NUMERIC(12,2) not null,
+    salario_ano8 NUMERIC(12,2) not null,
+    salario_ano9 NUMERIC(12,2) not null,
+    salario_ano10 NUMERIC(12,2) not null
+
 )
